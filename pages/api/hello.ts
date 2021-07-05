f714473d7a9e4880ae4ec1731c6980afde5f0d5d
KHong <albtrossfacefall@gmail.com>
2021-07-05 13:30:24 -0400

    convert files to typescript

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 // You can code API Routes/endpoints at pages/api
 // that can be deployed as Serverless Functions (Lambdas)
-export default function handler(req, res) {
+export default function handler(_:NextApiRequest, res: NextApiResponse) {
   // req is an instance of http.IncomingMessage, plus some middlewares
   // res is an instance of http.ServerResponse, plus some helper functions
   res.status(200).json({ text: "Hello" });
